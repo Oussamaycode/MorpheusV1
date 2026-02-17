b594df370753a57185c815fe2ba4220c173ec7da
Oussamaycode <oussamalamlouki047@gmail.com>
2026-02-17 17:34:30 +0000

    feat: add controllers, models, requests, policies, factories, migrations, and seeders for Friendship, Notification, Payment, and TournamentParticipant

---
+++ app/app/Http/Controllers/TournamentParticipantController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TournamentParticipant;
+use App\Http\Requests\StoreTournamentParticipantRequest;
+use App\Http\Requests\UpdateTournamentParticipantRequest;
+
+class TournamentParticipantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTournamentParticipantRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TournamentParticipant $tournamentParticipant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TournamentParticipant $tournamentParticipant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTournamentParticipantRequest $request, TournamentParticipant $tournamentParticipant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TournamentParticipant $tournamentParticipant)
+    {
+        //
+    }
+}
